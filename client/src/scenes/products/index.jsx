1e13d40c9ff1923b9bc987faf89652e5c6c8037c
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2022-12-22 04:11:59 +0100

    <\Products />: Error Handling

--- client/src/scenes/products/index.jsx
+++ client/src/scenes/products/index.jsx
@@ -86,52 +86,65 @@ const Product = ({
 };
 
 const Products = () => {
-  const { data, isLoading } = useGetProductsQuery();
+
   const isNonMobile = useMediaQuery("(min-width: 1000px)");
+  const { data, error, isLoading } = useGetProductsQuery();
+
+  if (error) {
+    return <>Error: {error.message}</>;
+  }
+
+  if (isLoading) {
+    return <>Loading...</>;
+  }
+
+  if (!data || data.length === 0) {
+    return <>No products found.</>;
+  }
 
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="PRODUCTS" subtitle="See your list of products." />
-      {data || !isLoading ? (
-        <Box
-          mt="20px"
-          display="grid"
-          gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-          justifyContent="space-between"
-          rowGap="20px"
-          columnGap="1.33%"
-          sx={{
-            "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
-          }}
-        >
-          {(data || []).map(
-            ({
-              _id,
-              name,
-              description,
-              price,
-              rating,
-              category,
-              supply,
-              stat,
-            }) => (
-              <Product
-                key={_id}
-                _id={_id}
-                name={name}
-                description={description}
-                price={price}
-                rating={rating}
-                category={category}
-                supply={supply}
-                stat={stat}
-              />
-            )
-          )}
-        </Box>
-      ) : (
-        <>Loading...</>
-      )}
+      {data && data.length > 0 ? (
+  <Box
+    mt="20px"
+    display="grid"
+    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+    justifyContent="space-between"
+    rowGap="20px"
+    columnGap="1.33%"
+    sx={{
+      "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+    }}
+  >
+    {data.map(
+      ({
+        _id,
+        name,
+        description,
+        price,
+        rating,
+        category,
+        supply,
+        stat,
+      }) => (
+        <Product
+          key={_id}
+          _id={_id}
+          name={name}
+          description={description}
+          price={price}
+          rating={rating}
+          category={category}
+          supply={supply}
+          stat={stat}
+        />
+      )
+    )}
+  </Box>
+) : (
+  <>Loading...</>
+)}
     </Box>
   );
 };
